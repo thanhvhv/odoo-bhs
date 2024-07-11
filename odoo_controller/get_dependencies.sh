eaaa977ecdbbdcfbdc7dfaf6778b740b122d8f22
thanhvhv <102660605+thanhvhv@users.noreply.github.com>
2024-07-11 13:39:14 +0700

    Update get_dependencies.sh

--- odoo_controller/get_dependencies.sh
+++ odoo_controller/get_dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PATH_ODOO_CONTROLLER=/home/thanhvhv/project/myodoo/odoo-bhs/odoo_controller
+PATH_ODOO_CONTROLLER=/home/bhsoft/odoo-bhs/odoo_controller
 ENTRYPOINT_SAMPLE="entrypoint.sh"
 APP=$1
 ADDONS=addons/$APP

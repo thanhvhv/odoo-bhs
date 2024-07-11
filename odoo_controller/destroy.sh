11b54adc7305a5cac446ce7a41b0293123268e38
thanhvhv <thanhvhv.bhsoft@gmai.com>
2024-07-11 09:56:04 +0700

    update destroy

--- odoo_controller/destroy.sh
+++ odoo_controller/destroy.sh
@@ -4,7 +4,7 @@ APP=$1
 
 cd $PATH_ODOO_CONTROLLER
 # Define the name of app
-docker exec -i $APP chown -R 1000:1000 /mnt/extra-addons/
+docker exec -i "$APP"_odoo_1 chown -R 1000:1000 /mnt/extra-addons/
 docker compose -p $APP down -v
 rm -r ./addons/$APP
 rm entrypoint_$APP.sh

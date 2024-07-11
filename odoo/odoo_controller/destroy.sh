9006cbbbb9783dccadc534ed30479ae211225170
thanhvhv <thanhvhv.bhsoft@gmai.com>
2024-07-11 15:08:22 +0700

    fix

--- odoo/odoo_controller/destroy.sh
+++ odoo/odoo_controller/destroy.sh
@@ -7,7 +7,7 @@ cd $PATH_ODOO_CONTROLLER
 docker exec -i "$APP"_odoo_1 chown -R 1000:1000 /mnt/extra-addons/
 docker exec -i "$APP"_odoo_1 chown 1000:1000 /entrypoint.sh 
 
-docker compose -p $APP down -v
+docker-compose -p $APP down -v
 rm -r ./addons/$APP
 rm entrypoint_$APP.sh
 sed -i "/$APP/d" ./port_data.txt
